ded3ae79f9a21e9b6d0d012368a70e658e79343c
Christian Centonze <cent0nZz@users.noreply.github.com>
2021-11-07 20:42:37 +0100

    dashboard style progress

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -2,14 +2,19 @@ import { Link } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import PlayedGamesRecap from '../played-games-recap/PlayedGamesRecap'
 
+import './Dashboard.scss'
+
 function Dashboard() {
   const currentUser = useSelector((state) => state.currentUser)
 
   return (
-    <div>
-      <h2>{currentUser.isLogged ? `Welcome ${currentUser.name}` : 'Welcome!'}</h2>
-      <Link to='/quiz'>Play Now</Link>
-      <Link to='/leaderboard'>Leaderboard</Link>
+    <div className="dashboard">
+      <h2 className="dashboard__welcome">{currentUser.isLogged ? `Welcome ${currentUser.name}!` : 'Welcome!'}</h2>
+      <p className="dashboard__description">Challenge yourself into this musical quiz game!</p>
+      <div className="dashboard__actions">
+        <Link className="dashboard__action dashboard__action--play" to='/quiz'>Play Now</Link>
+        <Link className="dashboard__action dashboard__action--leaderboard" to='/leaderboard'>Leaderboard</Link>
+      </div>
       {currentUser.isLogged && <PlayedGamesRecap />}
     </div>
   )
