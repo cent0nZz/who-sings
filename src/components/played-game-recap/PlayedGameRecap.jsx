1c87024b028b6c8d714db7fb4ac0e4d262178ddb
Christian Centonze <cent0nZz@users.noreply.github.com>
2021-11-07 20:54:15 +0100

    played games recap style progress

--- src/components/played-game-recap/PlayedGameRecap.jsx
+++ src/components/played-game-recap/PlayedGameRecap.jsx
@@ -1,9 +1,11 @@
+import './PlayedGameRecap.scss'
+
 function PlayedGameRecap(props) {
   return (
-    <div>
-      <div>#{props.id}</div>
-      <div>{new Date(props.timestamp).toLocaleString(navigator.language)}</div>
-      <div>+{props.totalPoints}pt.</div>
+    <div className="played-game-recap">
+      <div className="played-game-recap__id">#{props.id}</div>
+      <div className="played-game-recap__time">{new Date(props.timestamp).toLocaleString(navigator.language)}</div>
+      <div className="played-game-recap__points">+{props.totalPoints}pt.</div>
     </div>
   )
 }
--- src/components/played-games-recap/PlayedGamesRecap.jsx
+++ src/components/played-games-recap/PlayedGamesRecap.jsx
@@ -2,6 +2,8 @@ import { useSelector } from 'react-redux'
 import List from '../list/List'
 import PlayedGameRecap from '../played-game-recap/PlayedGameRecap'
 
+import './PlayedGamesRecap.scss'
+
 function PlayedGamesRecap() {
   const currentUser = useSelector((state) => state.currentUser)
   const playedGames =
@@ -9,7 +11,7 @@ function PlayedGamesRecap() {
       .map(playedGame => <PlayedGameRecap id={playedGame.id} timestamp={playedGame.timestamp} totalPoints={playedGame.totalPoints} />)
 
   return (
-    <div>
+    <div className="played-games-recap">
       <List items={playedGames} />
     </div>
   )
