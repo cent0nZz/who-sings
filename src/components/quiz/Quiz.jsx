a3b94861e361fbfe6bc054c62e459aefbe19fd6e
Christian Centonze <cent0nZz@users.noreply.github.com>
2021-11-07 22:09:06 +0100

    header style improvement

--- src/components/quiz/Quiz.jsx
+++ src/components/quiz/Quiz.jsx
@@ -5,23 +5,25 @@ import Modal from '../modal/Modal'
 import Game from '../game/Game'
 import { GameStates } from '../../redux/slices/currentGameSlice'
 
+import './Quiz.scss'
+
 function Quiz() {
   const currentGame = useSelector((state) => state.currentGame)
   const [showModal, setShowModal] = useState(false)
 
   return (
-    <div>
+    <div className="quiz">
       <Modal isActive={showModal} onClose={() => setShowModal(false)}>
         Are you sure you want to quit the current game? All the progress and stats will be lost!
-        <div>
-          <Link to='/'>Yes, quit</Link>
-          <button onClick={() => setShowModal(false)}>No, stay</button>
+        <div className="quit-quiz">
+          <Link className="quit-quiz__yes" to='/'>Yes, quit</Link>
+          <button className="quit-quiz__no" onClick={() => setShowModal(false)}>No, stay</button>
         </div>
       </Modal>
       {
         currentGame.gameState === GameStates.inGame
-          ? <button onClick={() => setShowModal(true)}>Back to Dashboard</button>
-          : <Link to='/'>Back to Dashboard</Link>
+          ? <button className="quiz__back style-as-anchor" onClick={() => setShowModal(true)}>Back to Dashboard</button>
+          : <Link className="quiz__back" to='/'>Back to Dashboard</Link>
       }
       <Game />
     </div>
