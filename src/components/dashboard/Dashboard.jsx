e600b574a7f48d36713f047aaffe2c9b322425db
Christian Centonze <cent0nZz@users.noreply.github.com>
2021-11-07 21:34:14 +0100

    play button logic with style

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -6,13 +6,21 @@ import './Dashboard.scss'
 
 function Dashboard() {
   const currentUser = useSelector((state) => state.currentUser)
+  const playButtonClass = currentUser.isLogged
+    ? 'dashboard__action dashboard__action--play'
+    : 'dashboard__action dashboard__action--play disabled'
 
   return (
     <div className="dashboard">
       <h2 className="dashboard__welcome">{currentUser.isLogged ? `Welcome ${currentUser.name}!` : 'Welcome!'}</h2>
-      <p className="dashboard__description">Challenge yourself into this musical quiz game!</p>
+      <p className="dashboard__description">
+        Challenge yourself into this musical quiz game.
+        {
+          !currentUser.isLogged && ' Log-In to play now!'
+        }
+      </p>
       <div className="dashboard__actions">
-        <Link className="dashboard__action dashboard__action--play" to='/quiz'>Play Now</Link>
+        <Link className={playButtonClass} to='/quiz'>Play Now</Link>
         <Link className="dashboard__action dashboard__action--leaderboard" to='/leaderboard'>Leaderboard</Link>
       </div>
       {currentUser.isLogged && <PlayedGamesRecap />}
