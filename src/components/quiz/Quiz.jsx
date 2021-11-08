e6b010d1710758a1a57631f8985366312775da3a
Christian Centonze <cent0nZz@users.noreply.github.com>
2021-11-08 12:43:52 +0100

    fix font family issue

--- src/components/quiz/Quiz.jsx
+++ src/components/quiz/Quiz.jsx
@@ -18,13 +18,13 @@ function Quiz() {
           Are you sure you want to quit the current game? All the progress and stats will be lost!
           <div className="quit-quiz__actions">
             <Link className="quit-quiz__action quit-quiz__action--yes" to='/'>Yes, quit</Link>
-            <button className="quit-quiz__action quit-quiz__action--no style-as-anchor" onClick={() => setShowModal(false)}>No, stay</button>
+            <button className="quit-quiz__action quit-quiz__action--no" onClick={() => setShowModal(false)}>No, stay</button>
           </div>
         </div>
       </Modal>
       {
         currentGame.gameState === GAME_STATES.inGame
-          ? <button className="quiz__back style-as-anchor" onClick={() => setShowModal(true)}>Back to Dashboard</button>
+          ? <button className="quiz__back" onClick={() => setShowModal(true)}>Back to Dashboard</button>
           : <Link className="quiz__back" to='/'>Back to Dashboard</Link>
       }
       <Game />
